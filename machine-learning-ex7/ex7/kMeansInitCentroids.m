8a9bf0cadde20148d47445c7e773cbfdcfdea1e8
mayank <mayankkvpy2014@gmail.com>
2018-01-12 16:42:38 +0530

    week 8 K-means

---
+++ machine-learning-ex7/ex7/kMeansInitCentroids.m
@@ -0,0 +1,30 @@
+function centroids = kMeansInitCentroids(X, K)
+%KMEANSINITCENTROIDS This function initializes K centroids that are to be 
+%used in K-Means on the dataset X
+%   centroids = KMEANSINITCENTROIDS(X, K) returns K initial centroids to be
+%   used with the K-Means on the dataset X
+%
+
+% You should return this values correctly
+centroids = zeros(K, size(X, 2));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: You should set centroids to randomly chosen examples from
+%               the dataset X
+%
+k=1;
+random_array=fix(rand(K,1)*size(X,1));
+for i=random_array'
+  centroids(k,:)=X(i,:);
+  k=k+1;
+endfor
+
+
+
+
+
+
+% =============================================================
+
+end
+
